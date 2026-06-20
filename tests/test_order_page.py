86374ff1c25ed3abbfec473cbee8ca38b12760fb
Dzhan Sina <bamaboy08@yandex.ru>
2026-06-20 20:40:33 +0300

    Тестовые данные для параметризации перенесены в отдельный файл order_page_data

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,25 +1,19 @@
 from selenium import webdriver  
-from selenium.webdriver.common.by import By
 from locators.order_page_locators import OrderPageLocators
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from data.urls import URL
 from pages.order_page import OrderPage
+from data.order_page_data import OrderPageData
 import pytest
 import allure 
 
 
 class TestOrderPage():
-    driver = None
 
-    @allure.title('Заказ самоката. Использование двух ноборов данных')
+    @allure.title('Заказ самоката. Использование двух наборов данных')
     @allure.description('Нажимаем на кнопку заказа самоката. Заполняем поля: Имя, Фамилия, Адрес, Станция метро, Телефон, Дата, Срок аренды, Цвет самоката, Комментарий')
-    @pytest.mark.parametrize('order_button_place, name, surname, address, station, phone, date, comment',
-        [
-        ('top', 'Джан', 'Сина','г.Москва, ул. Шарикоподшипниковская д.28, кв.110', 'Дубровка', '89995551122','01.07.2026', 'Домофон не работает'),
-        ('bottom','Роман','Рейнс','г.Москва, ул.Новоостаповская д.16, кв.304','Волгоградский проспект','89012736509','30.06.2026', 'Буду дома после 16:00')
-        ]
-    )
+    @pytest.mark.parametrize('order_button_place, name, surname, address, station, phone, date, comment', OrderPageData.Order_test_data)
     def test_make_order (self,driver,order_page,order_button_place,name, surname, address, station, phone, date,comment):
         order_page.make_order(order_button_place,name, surname, address, station, phone, date,comment)
         actual_header = order_page.get_text_of_header_success_order()
